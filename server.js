2d838883676d82071db01d1cc2efda54d1128b27
Phil Rowley <phirow@gmail.com>
2017-10-21 10:23:33 +0100

    Update server to take port from env

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
 const example = require('./routes/getExample');
 
+const port = process.env.PORT || 5000;
+
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
 
 app.get('/example', example);
 
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port);
+server.listen(port, function () {
+    console.log('Example app listening on port ' + server.address().port);
 });

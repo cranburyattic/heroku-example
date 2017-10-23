baf944511e11e6ef4217ef25552a71645759999f
Phil Rowley <phirow@gmail.com>
2017-10-23 10:30:39 +0100

    Add some GraphQL coolness

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 'use strict';
 
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 const http = require('http');
-
 const example = require('./routes/getExample');
-const def = require('./routes/getDefault');
 
 const port = process.env.PORT || 5000;
 
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+
 const app = express();
-const server = http.createServer(app);
 
 app.get('/example', example);
-app.get('/', def);
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+
+const server = http.createServer(app);
 
 server.listen(port, function () {
     console.log('Example app listening on port ' + server.address().port);
